f93907227aacd65d0308166f46a6585c896a412d
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2021-01-09 17:08:25 -0500

    Delete Boilerplate Code

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+/*
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +26,4 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+*/
